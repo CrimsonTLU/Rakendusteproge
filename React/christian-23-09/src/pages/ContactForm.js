ab6e4afb11541a68c8a91d8203355754e2d72725
Christian-Enrique Hindremäe <chris55@tlu.ee>
2022-09-24 14:42:43 +0300

    kodutöö3-ga tegelemine

--- React/christian-23-09/src/pages/ContactForm.js
+++ React/christian-23-09/src/pages/ContactForm.js
@@ -1,7 +1,33 @@
-import { TextField } from "@mui/material"
+import { Box, TextField, Typography, Button } from "@mui/material"
 
 const ContactForm = () => {
-  return <TextField>Contact Form</TextField>
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "left",
+        justifyContent: "top",
+        flexDirection: "column",
+        gap: "25px",
+        height: "100vh",
+        width: "50vw"
+      }}
+    >
+      <Typography variant="h2">Contact Form</Typography>
+      <Typography variant="h3">Name</Typography>
+      <TextField></TextField>
+      <Typography variant="h3">Email</Typography>
+      <TextField></TextField>
+      <Typography variant="h3">Address</Typography>
+      <TextField></TextField>
+      <Button
+        color="success"
+        variant="contained"
+      >
+        Send
+      </Button>
+    </Box>
+  )
 }
 
 export default ContactForm
--- React/christian-23-09/src/pages/Table.js
+++ React/christian-23-09/src/pages/Table.js
@@ -6,7 +6,17 @@ import Box from "@mui/material/Box"
 
 const Table = () => {
   return (
-    <Box>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "left",
+        justifyContent: "top",
+        flexDirection: "column",
+        gap: "25px",
+        height: "100vh",
+        width: "50vw"
+      }}
+    >
       <ListItem>
         <ListItemButton>
           <ListItemText>Tere</ListItemText>
