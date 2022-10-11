572f06c65c72238c7b26a742bed43fc2adcb132f
CrimsonTLU <chris55@tlu.ee>
2022-10-11 19:16:01 +0300

    07-10 kodutöö

--- React/30-09/server/index.js
+++ React/30-09/server/index.js
@@ -1,16 +1,19 @@
 // ES5 vs ES6 import
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 const app = express()
 const PORT = 8080
+require('dotenv')
+
 const catsRoutes = require('./routes/cats.routes.js')
 const dogsRoutes = require('./routes/dogs.routes.js')
-require('dotenv')
+const authRoutes = require('./routes/auth.routes')
 
 app.use(morgan('dev'))
 app.use(express.json()) // body-parser asemel
 
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}...`
+const uri = "mongodb+srv://chris:<password>@cluster0.u49hmd4.mongodb.net/?retryWrites=true&w=majority"
 mongoose
   .connect(uri)
   .then(() => console.log('Database connection established'))
@@ -18,6 +21,7 @@ mongoose
 
 app.use('/cats', catsRoutes)
 app.use('/dogs', dogsRoutes)
+app.use('/auth', authRoutes)
 
 // CRUD
 app.get('/', (req, res) => {
